ee4321cfa19fa618abace617aae7dd40d99d6024
Dhruv-Varshney55 <dhruvvarshney24112002@gmail.com>
2025-01-17 14:00:34 +0530

    first commit

---
+++ Largest.java
@@ -0,0 +1,14 @@
+package Largest;
+
+public class Largest {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        if(a > b){
+            System.out.println(a);
+        }
+        else{
+            System.out.println(b);
+        }
+    }
+}
