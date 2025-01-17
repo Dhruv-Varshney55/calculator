cbddc939a82d492b9f4340b177b8cd34cd702853
Dhruv-Varshney55 <dhruvvarshney24112002@gmail.com>
2025-01-17 13:47:46 +0530

    first commit

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+package Multiplication;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int x = a * b;
+        System.out.println("Multiplication = " + x);
+    }
+}
