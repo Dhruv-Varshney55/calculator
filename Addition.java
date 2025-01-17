71a680a8418d3d0ce36ac0e54fce5533b57d7781
Dhruv-Varshney55 <dhruvvarshney24112002@gmail.com>
2025-01-17 13:38:03 +0530

    first commit

---
+++ Addition.java
@@ -0,0 +1,9 @@
+package Addition;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("Sum = " + a + b);
+    }
+}
