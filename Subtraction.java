68cea238499e04e81dabac72f073756786bd1c32
Dhruv-Varshney55 <dhruvvarshney24112002@gmail.com>
2025-01-17 13:41:39 +0530

    first commit

---
+++ Subtraction.java
@@ -0,0 +1,10 @@
+package Subtraction;
+
+public class Subtraction {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 20;
+        int x = a - b;
+        System.out.println("Sum = " + x);
+    }
+}
