406e6730b8185238123061b76cce908494d428b0
Dhruv-Varshney55 <dhruvvarshney24112002@gmail.com>
2025-01-17 14:02:38 +0530

    first commit

---
+++ Smallest.java
@@ -0,0 +1,14 @@
+package Smallest;
+
+public class Smallest {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        if(a < b){
+            System.out.println(a);
+        }
+        else{
+            System.out.println(b);
+        }
+    }
+}
